dd296591f62a82641ddde8815be845bada16106e
lazuniga03.lazuniga03 <lazuniga03.lualzuvi@gmail.com>
2023-09-29 20:09:28 -0600

    Examen extracción de data de Ebay y subida a Mongo

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from mongo import MongoConnection
 
 
 db_client = MongoConnection().client
-db = db_client.get_database('despegar')
-col = db.get_collection('flights')
+db = db_client.get_database('ebay')
+col = db.get_collection('relojes')
 
 driver = webdriver.Chrome()
 driver.get("https://www.ebay.com/e/latam/luxurywatches?_trkparms=pageci%3Adccab2fb-5f2e-11ee-9e84-f6ec8a7d2116%7Cparentrq%3Ae3a7c31f18a0a56184d7cfe3fffe68ce%7Ciid%3A1")
 
-flights = driver.find_elements(By.CLASS_NAME, "s-item__wrapper clearfix")
+flights = driver.find_elements(By.CLASS_NAME, "s-item__wrapper clearfix") #conunto de image,valores
 for f in flights:
     nombre_articulo = f.find_element(by=By.CLASS_NAME, value="s-item__title").text
     precio = f.find_element(by=By.CLASS_NAME, value="s-item__trending-price").text
